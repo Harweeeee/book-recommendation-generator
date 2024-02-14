220253e596a50f99bc6aa6e462ba5f381fb6feff
Harleigh <155698437+Harweeeee@users.noreply.github.com>
2024-02-14 15:05:51 +1000

    Added JS functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+let formElement = document.querySelector('form')
+formElement.addEventListener('submit', displayBook)
 
+function displayBook(event) {
+    event.preventDefault()
+
+    new Typewriter('#book-description', {
+        strings: 'hi',
+        autoStart: true,
+        delay: 1,
+    })
+}
