ded4b90cde4b4d3659e836b2569749aca41643aa
Harleigh <155698437+Harweeeee@users.noreply.github.com>
2024-02-14 15:48:53 +1000

    Integrated API and made it work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 let formElement = document.querySelector('form')
-formElement.addEventListener('submit', displayBook)
+formElement.addEventListener('submit', generateBook)
 
-function displayBook(event) {
+let userInput = document.querySelector('#text-input')
+let apiKey = '263tdf1c5f75o8ab465cedabbe0f4604'
+let apiContext =
+    'You are a specialist in all things regarding novels, books, you have lots of knowledge on different book categories, genres and authors.'
+let apiPrompt = `Provide a book recommendation in the ${userInput.value} genre. Provide the book title at the top of the paragraph in a <strong>. After a <br> describe the book content. The book suggested must be in the genre the user wants`
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`
+let bookDescription = document.querySelector('#book-description')
+
+function generateBook(event) {
     event.preventDefault()
 
-    new Typewriter('#book-description', {
-        strings: 'hi',
-        autoStart: true,
-        delay: 1,
-    })
+    bookDescription.innerHTML = 'Generating book suggestion...'
+    axios.get(apiUrl).then(typeBook)
+
+    function typeBook(response) {
+        new Typewriter(bookDescription, {
+            strings: response.data.answer,
+            autoStart: true,
+            delay: 1,
+        })
+    }
 }
